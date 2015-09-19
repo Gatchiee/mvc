424cae54b706ef030da390577f8d5a212fd393ff
maqnifique <dhes.gatchalian@gmail.com>
2015-09-19 22:17:12 +0800

    Update Employee_update.php

--- Employee_update.php
+++ Employee_update.php
@@ -5,13 +5,17 @@
 </head>
 <body>
 <script>
-	function confirmSubmit(){
-		if(confirm('Are you sure you want to update this Employee?')){
-			window.alert("Information is now updated! Go to Display Employees to see changes.");
-			return true;
+function confirmSubmit(){
+	var ar= [document.addform.empID.value, document.addform.empName.value, document.addform.empAge.value, document.addform.empAddress.value, document.addform.empPosition.value];
+	var check=true;
+	for(var i=0;i<5;i++){
+	if((ar[i]).length==0){
+		check=false;
 		}
-		return false;
 	}
+	if(check)
+		alert("Information is now updated! Go to Display Employees to see changes.");
+		}
 </script>
 <div id="wrapper">
 	<div id="header">
@@ -33,21 +37,21 @@
 		<form action="" method="post">
 				<h1>Update Employee</h1>
 				<p>Employee ID<br />
-					<input type="text" value="<?php echo $_GET['id'];?>" name="empID" readonly /></p>
+					<input type="text" value="<?php echo $_GET['id'];?>" name="empID" readonly="readonly" /></p><br />
 				<p>Employee Name<br />
-					<input type="text" name="empName" required /></p>
+					<input type="text" value="<?php echo $_GET['name'];?>" name="empName" required /></p><br />
 				<p>Employee Age<br />
-					<input type="text" name="empAge" required onkeypress="if ( isNaN( String.fromCharCode(event.keyCode) )){ alert('Numbers only!!'); return false; }" /></p>
+					<input type="text" name="empAge" required onkeypress="if ( isNaN( String.fromCharCode(event.keyCode) )){ alert('Numbers only!!'); return false; }" /></p><br />
 				<p>Employee Address<br />
-					<input type="text" name="empAddress" required /></p>
+					<input type="text" name="empAddress" required /></p><br />
 				<p>Employee Position<br />
-					<input type="text" name="empPosition" required /></p>
+					<input type="text" name="empPosition" required /></p><br />
 			<p>
-				<input type="submit" value="Update" onclick="return confirmSubmit();"/>
+				<input type="submit" value="Update" onclick="confirmSubmit();"/>
 				<input type="reset" value="Clear" />
-			</p>
+			</p><br />
 			<p><a href="employees.php">Back to Home<a/></p>
-			
+
 		</form>
 	</div>
 	
